2363c9cb0cd0d1602411c5bd0536ff835b03c597
waj949 <wajdihamrouni1993@gmail.com>
2019-12-04 14:30:50 +0100

    beeesbeees

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,16 @@
 var ForagerBee = function() {
+	//  call the Bee superclass
+	Bee.call(this);
+	this.age = 10;
+	this.job =  "find pollen"
+	this.canFly = true;
+	this.treasureChest = [];
+ // a forage method that allows the bee to add a treasure to the treasureChest
 };
+ForagerBee .prototype = Object.create(Bee.prototype);
 
+ForagerBee .prototype.constructor = ForagerBee ;
+
+ForagerBee .prototype.forage = function(treasure){
+	this.treasureChest.push(treasure);
+}
--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,19 @@
 var HoneyMakerBee = function() {
-};
+		// call the Bee superclass
+		Bee.call(this);
+		this.age = 10;
+		this.job = "make honey";
+		this.honeyPot = 0;
+}
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
 
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+
+HoneyMakerBee.prototype.makeHoney = function(){
+	this.honeyPot += 1;
+	console.log(this.color);
+}
+
+HoneyMakerBee.prototype.giveHoney = function(){
+	this.honeyPot -= 1;
+}
